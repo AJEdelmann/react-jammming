d7123588fbb46ee5dd15dbea650143b12cb11c69
ajedelmann <albert.edelmann@gmail.com>
2019-12-21 22:01:45 +0100

    Passed down Playlist to Tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,6 +27,27 @@ class App extends React.Component {
           album: "album3",
           id: 3
         }
+      ],
+      playlistName: "my playlist name",
+      playlistTracks: [
+        {
+          name: "playlistName1",
+          artist: "playlistArtist1",
+          album: "playlistAlbum1",
+          id: 4
+        },
+        {
+          name: "playlistName2",
+          artist: "playlistArtist2",
+          album: "playlistAlbum2",
+          id: 5
+        },
+        {
+          name: "playlistName3",
+          artist: "playlistArtist3",
+          album: "playlistAlbum3",
+          id: 6
+        }
       ]
     };
   }
@@ -40,7 +61,10 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+            />
           </div>
         </div>
       </div>
