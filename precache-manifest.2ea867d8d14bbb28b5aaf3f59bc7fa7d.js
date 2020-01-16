4021386de4eb0eeaa8a8ecb1c1834479cc068880
ajedelmann <albert.edelmann@gmail.com>
2020-01-16 22:38:44 +0100

    Updates

---
+++ precache-manifest.2ea867d8d14bbb28b5aaf3f59bc7fa7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e7bffc2f5a66706af1b034a352a77c",
+    "url": "/react-jammming/index.html"
+  },
+  {
+    "revision": "3efa48f8a00451cd05c7",
+    "url": "/react-jammming/static/css/main.85b4954a.chunk.css"
+  },
+  {
+    "revision": "389e0ead39efbd694202",
+    "url": "/react-jammming/static/js/2.5fa17389.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-jammming/static/js/2.5fa17389.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3efa48f8a00451cd05c7",
+    "url": "/react-jammming/static/js/main.945c0853.chunk.js"
+  },
+  {
+    "revision": "c25b352da8ba5ea1fb85",
+    "url": "/react-jammming/static/js/runtime-main.0352d4a8.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/react-jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
